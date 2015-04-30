8a93c3590d1b22e46a6114eabbd9866c6a0fd8bd
Colm Austin <colm@daft.ie>
2015-04-30 16:47:29 +0100

    add the php conf files

--- puppet/manifests/classes/php5-fpm.pp
+++ puppet/manifests/classes/php5-fpm.pp
@@ -1,14 +1,41 @@
 class php5-fpm {
 
-   package { 'php5-fpm':
-       ensure => installed
-   }
+    package { 'php5-fpm':
+        ensure => installed
+    }
 
-   service { 'php5-fpm':
-       enable => true,
-       ensure => running,
-       hasrestart => true,
-       hasstatus => true,
-   }
+    service { 'php5-fpm':
+        enable => true,
+        ensure => running,
+        hasrestart => true,
+        hasstatus => true,
+    }
+
+    file { "/etc/php5/fpm/php.ini":
+        source => "puppet:///modules/helloworld/etc/php5/fpm/php.ini",
+        ensure => file,
+        owner => root,
+        group => root,
+        mode => 644,
+        notify => service["php5-fpm"],
+    }
+
+    file { "/etc/php5/fpm/php-fpm.conf":
+        source => "puppet:///modules/helloworld/etc/php5/fpm/php-fpm.conf",
+        ensure => file,
+        owner => root,
+        group => root,
+        mode => 644,
+        notify => service["php5-fpm"],
+    }
+
+    file { "/etc/php5/fpm/pool.d/www.conf":
+        source => "puppet:///modules/helloworld/etc/php5/fpm/pool.d/www.conf",
+        ensure => file,
+        owner => root,
+        group => root,
+        mode => 644,
+        notify => service["php5-fpm"],
+    }
 
 }
